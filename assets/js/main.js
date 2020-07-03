a9aa327c215ade6c0fd74cd06caa89d39844717f
akm dolar <1morelearn@gmail.com>
2020-07-03 18:52:31 +0600

    Educafe v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>'],
+        autoplay: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    
+    
+    
+
+    
+    
+    
+    
+    
+
+});
